aa993e9dc97076928402b58c41484addfbb43e30
Amit Panwar <amitpanwar02705@gmail.com>
2022-12-02 15:08:11 +0530

    initial clone

---
+++ src/Banner.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axiosReq from "./axios";
+import requests from "./request";
+import "./banner.css"
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+export default function Banner() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const requestData = await axiosReq.get(requests.fetchTrending);
+      setMovie(
+        requestData.data.results[
+          Math.floor(Math.random() * requestData.data.results.length)
+        ]
+      );
+      return requestData;
+    }
+    fetchData();
+  }, []);
+  
+  function truncate(str,n){
+    return str?.length > n ? str.substr(0,n-1)+"...":str;
+  }
+
+  return (
+    <header className="banner" style={{
+        backgroundSize : "cover",
+        backgroundImage : `url(${baseUrl}${movie?.backdrop_path})`,
+        backgroundPosition : "center center"
+    }}>
+      <div className="bannerContents">
+        <h1 className="bannerTitle">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="bannerButtons">
+          <button className="bannerButton">Play</button>
+          <button className="bannerButton">My List</button>
+        </div>
+        <h2 className="bannerDescription">{truncate(movie?.overview , 150)}</h2>
+      </div>
+      <div className="bannerFadeBottom"></div>
+    </header>
+  );
+}
---
+++ src/Row.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axiosReq from "./axios";
+import "./row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  //this code runs based on the conditions
+  // [] => runs once when the component loads first time
+  useEffect(() => {
+    async function fetchData() {
+      const requestData = await axiosReq.get(fetchUrl);
+      // setMovies(requestData.data.results);
+      //remove the result without posters
+      const temp = requestData.data.results.filter((movie)=>{
+        if(movie.backdrop_path) return true;
+        return false;
+      });
+      setMovies(temp);
+      console.log(movies[0]?.backdrop_path)
+      return requestData;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  //[fetchUrl] , component depends upon fetchUrl, whenever it changes we need to reload it
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="rowPosters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`poster ${isLargeRow && "rowPosterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie?.name || movie?.title || movie?.original_title}
+          ></img>
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}></Youtube>}
+    </div>
+  );
+}
+
+export default Row;
